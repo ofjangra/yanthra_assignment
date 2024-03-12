cab1c9a3cbbbe54ffcf98dee45673c50e37f043e
ofjangra <zannrez@gmail.com>
2024-03-12 13:42:33 +0530

    chat modal ref added

--- app/components/ChatModal.tsx
+++ app/components/ChatModal.tsx
@@ -8,6 +8,7 @@ import { useDispatch } from "react-redux"
 import SmileEmoji from "@/public/vector/Smile.svg"
 import Attachments from "@/public/vector/Attachment.svg"
 import Mic from "@/public/vector/Mic.svg"
+import { useEffect, useRef } from "react"
 
 const ChatModal = () => {
     const state = useSelector<RootState>((state) => state.appSlice) as AppState
@@ -17,13 +18,28 @@ const ChatModal = () => {
     const chatsData = state.chats
     const userId = state.userID
 
+    const popupRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        function handleClickOutside(event: MouseEvent) {
+            if (popupRef.current && !popupRef.current.contains(event.target as Node)) {
+                dispatch(handleChatModal(false))
+            }
+        }
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [dispatch]);
+
     if (!state.chatModalActive) {
         return null
     }
     return (
         <>
             <div className="modal_container fixed top-0 left-0 w-full">
-                <div className="Chat_container flex flex-col items-center justify-between">
+                <div className="Chat_container flex flex-col items-center justify-between" ref={popupRef}>
                     <div className="chat_title flex flex-row items-center justify-between">
                         <div className="chat_user_info flex flex-row items-center justify-between">
                             <Image src={chatsData.senderProfileImage} alt="user" width={40} height={40} className="sender_title_image" />
